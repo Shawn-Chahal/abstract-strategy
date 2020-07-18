98e5b06210962d75f781e49a3955ece5e47af42c
Shawn-Chahal <shawn.chahal@gmail.com>
2020-07-18 02:13:02 -0400

    Created ConnectFour class

--- abstract-strategy.cpp
+++ abstract-strategy.cpp
@@ -8,37 +8,38 @@ int main() {
     
     int play_again = 1;
     
+    
+
     while (play_again == 1) {
 
-        std::vector<std::vector<int>> game_state (N_ROW, std::vector<int> (N_COL, 0));
-        std::vector<int> avail_col (N_COL, 1);
-        std::vector<int> difficulty = {6, 8, 10};
+        ConnectFour game;
+
         double score, max_score;
         int col, d_index;
         int result = -1;
         int player = 1;
         char s_play_again;
         std::default_random_engine generator;
-        std::uniform_int_distribution<int> distribution(0, N_COL - 1);        
+        std::uniform_int_distribution<int> distribution(0, game.N_MOVES - 1);        
 
         std::cout << std::endl << std::endl;
-        std::cout << INDENT << "Connect Four" << std::endl << std::endl;
-        std::cout << INDENT << "Select a difficulty." << std::endl << std::endl;
-        std::cout << INDENT << "1) Easy" << std::endl;
-        std::cout << INDENT << "2) Medium" << std::endl;
-        std::cout << INDENT << "3) Hard" << std::endl << std::endl;
-        std::cout << INDENT << "Enter a number: ";
+        std::cout << game.INDENT << game.NAME << std::endl << std::endl;
+        std::cout << game.INDENT << "Select a difficulty." << std::endl << std::endl;
+        std::cout << game.INDENT << "1) Easy" << std::endl;
+        std::cout << game.INDENT << "2) Medium" << std::endl;
+        std::cout << game.INDENT << "3) Hard" << std::endl << std::endl;
+        std::cout << game.INDENT << "Enter a number: ";
 
         d_index = -1;
 
         while (d_index == -1) {
             if (!(std::cin >> d_index)){
-                std::cout << INDENT << "Invalid input. Please enter a number: ";
+                std::cout << game.INDENT << "Invalid input. Please enter a number: ";
                 std::cin.clear();
                 std::cin.ignore(10000,'\n');
                 d_index = -1;
-            } else if (d_index < 1 || d_index > difficulty.size()) {
-                std::cout << INDENT << "Difficulty unavailable. Please enter a number between 1 and " << difficulty.size() << ": ";
+            } else if (d_index < 1 || d_index > game.difficulty.size()) {
+                std::cout << game.INDENT << "Difficulty unavailable. Please enter a number between 1 and " << game.difficulty.size() << ": ";
                 std::cin.clear();
                 std::cin.ignore(10000,'\n');
                 d_index = -1;
@@ -46,47 +47,47 @@ int main() {
         }        
        
         std::cout << std::endl << std::endl;
-        std::cout << INDENT << "Player markers" << std::endl;
-        std::cout << INDENT << "You:      " << P1_MARKER << std::endl;
-        std::cout << INDENT << "Computer: " << P2_MARKER << std::endl << std::endl;
+        std::cout << game.INDENT << "Player markers" << std::endl;
+        std::cout << game.INDENT << "You:      " << game.P1_MARKER << std::endl;
+        std::cout << game.INDENT << "Computer: " << game.P2_MARKER << std::endl << std::endl;
 
-        print_board(game_state, avail_col);
+        game.print_board(game.game_state, game.available_moves);
 
         while (result == -1) {
 
             if (player == 1) {
-                std::cout << INDENT << "Your turn. Enter a number: ";
+                std::cout << game.INDENT << "Your turn. Enter a number: ";
                 col = -1;
 
                 while (col == -1) {
                     if (!(std::cin >> col)){
-                        std::cout << INDENT << "Invalid input. Please enter a number: ";
+                        std::cout << game.INDENT << "Invalid input. Please enter a number: ";
                         std::cin.clear();
                         std::cin.ignore(10000,'\n');
                         col = -1;
-                    } else if (!check_input(col, avail_col)) {
-                        std::cout << INDENT << "Move unavailable. Please enter an available number:  ";
+                    } else if (!game.check_input(col)) {
+                        std::cout << game.INDENT << "Move unavailable. Please enter an available number:  ";
                         std::cin.clear();
                         std::cin.ignore(10000,'\n');
                         col = -1;
                     }
                 }              
             } else {
-                std::cout << INDENT << "Computer is thinking.";
-                std::vector<int> attempted_moves (N_COL, 0);
-                max_score = -S_INITIAL;
+                std::cout << game.INDENT << "Computer is thinking.";
+                std::vector<int> attempted_moves = std::vector<int>(game.N_MOVES, 0);
+                max_score = -game.S_INITIAL;
 
-                while (!check_moves(attempted_moves)) {
+                while (!game.check_moves(attempted_moves)) {
                     int j = distribution(generator);
 
-                    if (j >=0 && j < N_COL){
+                    if (j >=0 && j < game.N_MOVES){
                         if (!attempted_moves[j]) {
 
                             attempted_moves[j] = 1;
                             std::cout << ".";
                             
-                            if (avail_col[j] == 1) {
-                                score = get_score(game_state, avail_col, player, j, 1, difficulty[d_index - 1], max_score);
+                            if (game.available_moves[j] == 1) {
+                                score = game.get_score(game.game_state, game.available_moves, player, j, 1, game.difficulty[d_index - 1], max_score);
                 
                                 if (score > max_score) {
                                     max_score = score;
@@ -98,35 +99,34 @@ int main() {
                 }
                 
                 std::cout << std::endl;
-                std::cout << INDENT << "Computer's move: " << col << std::endl;
+                std::cout << game.INDENT << "Computer's move: " << col << std::endl;
             }
 
             std::cout << std::endl;
             
-            game_state = update_state(game_state, player, col);
-            avail_col = update_avail_col(game_state, avail_col);
-            print_board(game_state, avail_col);
-            result = check_state(game_state);
-            player = switch_player(player);
+
+            game.end_turn(player, col);
+            result = game.check_state(game.game_state);
+            player = game.switch_player(player);
         }
         
         switch (result) {
 
             case 0:
-                std::cout << INDENT << "Draw." << std::endl;
+                std::cout << game.INDENT << "Draw." << std::endl;
                 break;
 
             case 1:
-                std::cout << INDENT << "You win!" << std::endl;
+                std::cout << game.INDENT << "You win!" << std::endl;
                 break;
 
             case 2:
-                std::cout << INDENT << "You lose." << std::endl;
+                std::cout << game.INDENT << "You lose." << std::endl;
                 break;
         }
 
         do {
-            std::cout << INDENT << "Play again? Y/N: ";
+            std::cout << game.INDENT << "Play again? Y/N: ";
             std::cin >> s_play_again;
             std::cin.clear();
             std::cin.ignore(10000,'\n');
--- connect-four.h
+++ connect-four.h
@@ -2,261 +2,289 @@
 #include <vector>
 #include <cmath>
 
-const int N_ROW = 6;
-const int N_COL = 7;
-const int CONNECT = 4;
-const double S_INITIAL = 1000000.0;
-const double DECAY = 0.9;
-const char P1_MARKER = 'O';
-const char P2_MARKER = 'X';
-const std::string INDENT = "   ";
 
-int check_moves(std::vector<int> attempted_moves) {
 
-    for (int i = 0; i < attempted_moves.size(); i++) {
-        if (attempted_moves[i] == 0) {
-            return 0;
+class ConnectFour {
+    private:
+        /* data */
+    public:
+        
+        const int N_ROW = 6;
+        const int N_COL = 7;
+        const int N_MOVES = N_COL;
+        const int CONNECT = 4;
+        const double S_INITIAL = 1000000.0;
+        const double DECAY = 0.9;
+        int player = 1;
+        int result = -1;
+        const char P1_MARKER = 'O';
+        const char P2_MARKER = 'X';
+        const std::string INDENT = "   ";
+        const std::string NAME = "Connect Four";
+        std::vector<std::vector<int>> game_state = std::vector<std::vector<int>>(N_ROW, std::vector<int>(N_COL, 0));
+        std::vector<int> available_moves = std::vector<int>(N_MOVES, 1);
+        std::vector<int> difficulty = {6, 8, 10};
+
+                
+        int check_moves(std::vector<int> attempted_moves) {
+
+            for (int i = 0; i < attempted_moves.size(); i++) {
+                if (attempted_moves[i] == 0) {
+                    return 0;
+                }
+            }
+
+            return 1;
         }
-    }
-
-    return 1;
-}
 
 
-int check_input(int col, std::vector<int> avail_col) {
-   
-    if (col >= 0 && col < N_COL) {
-        return avail_col[col];
-    } else {
-        return 0;
-    }
-}
-
-
-int switch_player(int player) {
-    
-    if (player == 1) {
-        return 2;
-    } else {
-        return 1;
-    }
-}
-
-
-void print_board(std::vector<std::vector<int>> game_state, std::vector<int> avail_col) {
-    
-    std::cout << INDENT;
-    
-    for (int j = 0; j < N_COL; j++) {
-        if (avail_col[j] == 1) {
-            std::cout << "  " << j << " ";
-        } else {
-            std::cout << "    ";
-        }
-    }
-
-    std::cout << " " << std::endl;
-    
-    for (int i = 0; i < N_ROW; i++) {
-        std::cout << INDENT;
-        for (int j = 0; j < N_COL; j++) {
-            switch (game_state[i][j]) {
-                case 0:
-                    std::cout << "|   ";
-                    break;
-                case 1:
-                    std::cout << "| " << P1_MARKER << " ";
-                    break;
-                case 2:
-                    std::cout << "| " << P2_MARKER << " ";
-                    break;
+        int check_input(int col) {
+        
+            if (col >= 0 && col < N_MOVES) {
+                return available_moves[col];
+            } else {
+                return 0;
             }
         }
 
-        std::cout << "|" << std::endl;
-        std::cout << INDENT;
 
-        for (int j = 0; j < N_COL; j++) {
-            std::cout << "----";
-        }
-
-        std::cout << "-" << std::endl;
-    }
-}
-
-
-std::vector<std::vector<int>> update_state(std::vector<std::vector<int>> game_state, int player, int col) {
-
-    for (int row = N_ROW - 1; row >= 0; row--) {
-        if (game_state[row][col] == 0) {
-            game_state[row][col] = player;
-            break;
-        }
-    }   
-    return game_state;
-}
-
-
-std::vector<int> update_avail_col(std::vector<std::vector<int>> game_state, std::vector<int> avail_col) {
-    
-    for (int j=0; j < N_COL; j++) {
-        if (game_state[0][j] > 0) {
-            avail_col[j] = 0;
-        }
-    }
-    return avail_col;
-}
-
-
-int check_state(std::vector<std::vector<int>> game_state) {
-
-    int player, count;
-    int check_draw = 0;
-
-    for (int i = 0; i < N_ROW; i++) {
-        for (int j = 0; j < N_COL; j++) {
-            if (game_state[i][j] > 0) {
-                if (i == 0) {
-                    check_draw++;
-                }
-
-                player = game_state[i][j];
-
-                /* Check right */
-                if (j <= N_COL - CONNECT) {
-                    count = 1;
-
-                    for (int k = 1; k < CONNECT; k++) {
-                        if (game_state[i][j + k] == player) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                    }
-                    
-                    if (count == CONNECT) {
-                        return player;
-                    }
-                }
-
-                /* Check down */
-                if (i <= N_ROW - CONNECT) {
-                    count = 1;
-
-                    for (int k = 1; k < CONNECT; k++) {
-                        if (game_state[i + k][j] == player) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if (count == CONNECT) {
-                        return player;
-                    }
-                }
-
-                /* Check right-down */
-                if (i <= N_ROW - CONNECT && j <= N_COL - CONNECT) {
-                    count = 1;
-                    
-                    for (int k = 1; k < CONNECT; k++) {
-                        if (game_state[i + k][j + k] == player) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                    }
-                    
-                    if (count == CONNECT) {
-                        return player;
-                    }
-                }
-
-                /* Check left-down */
-                if (i <= N_ROW - CONNECT && j >= CONNECT - 1) {
-                    count = 1;
-                    
-                    for (int k = 1; k < CONNECT; k++) {
-                        if (game_state[i + k][j - k] == player) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if (count == CONNECT) {
-                        return player;
-                    }
-                }
-            }
-        }
-    }
-
-    if (check_draw == N_COL) {
-        /* Declare a draw */
-        return 0;
-    } else {
-        /* Game continues */
-        return -1;
-    }
-}
-
-
-double get_score(std::vector<std::vector<int>> game_state, std::vector<int> avail_col, int player, int col, int depth, int max_depth, double ab) {
-
-    game_state = update_state(game_state, player, col);
-    avail_col = update_avail_col(game_state, avail_col);
-    int result = check_state(game_state);
-    double score;
-
-    if (result == 2) {
-        return pow(DECAY, depth);
-    } else if (result == 1) {  
-        return -pow(DECAY, depth);
-    } else if (result == 0) {
-        return 0.0;
-    } else if (depth == max_depth) {  
-        return 0.0;
-    } else {
-        player = switch_player(player);
-
-        if (player == 1) {
+        int switch_player(int player) {
             
-            double min_score = S_INITIAL;
-
-            for (int j = 0; j < N_COL; j++) {
-                if (avail_col[j] == 1) {
-                    score = get_score(game_state, avail_col, player, j, depth + 1, max_depth, min_score);
-
-                    if (score < min_score) {
-                        min_score = score;
-                    }
-
-                    if (min_score <= ab) {
-                        break;
-                    }
-                }
+            if (player == 1) {
+                return 2;
+            } else {
+                return 1;
             }
-            return min_score;
-        }  else /* if (player == 2) */ {
-
-            double max_score = -S_INITIAL;
-
-            for (int j = 0; j < N_COL; j++) {
-                if (avail_col[j] == 1) {
-                    score = get_score(game_state, avail_col, player, j, depth + 1, max_depth, max_score);
-
-                    if (score > max_score) {
-                        max_score = score;
-                    }
-
-                    if (max_score >= ab) {
-                        break;
-                    }
-                }
-            }
-            return max_score;
         }
-    }
-}
+
+
+        void print_board(std::vector<std::vector<int>> game_state, std::vector<int> available_moves) {
+            
+            std::cout << INDENT;
+            
+            for (int j = 0; j < N_MOVES; j++) {
+                if (available_moves[j] == 1) {
+                    std::cout << "  " << j << " ";
+                } else {
+                    std::cout << "    ";
+                }
+            }
+
+            std::cout << " " << std::endl;
+            
+            for (int i = 0; i < N_ROW; i++) {
+                std::cout << INDENT;
+                for (int j = 0; j < N_COL; j++) {
+                    switch (game_state[i][j]) {
+                        case 0:
+                            std::cout << "|   ";
+                            break;
+                        case 1:
+                            std::cout << "| " << P1_MARKER << " ";
+                            break;
+                        case 2:
+                            std::cout << "| " << P2_MARKER << " ";
+                            break;
+                    }
+                }
+
+                std::cout << "|" << std::endl;
+                std::cout << INDENT;
+
+                for (int j = 0; j < N_COL; j++) {
+                    std::cout << "----";
+                }
+
+                std::cout << "-" << std::endl;
+            }
+        }
+
+
+        std::vector<std::vector<int>> update_state(std::vector<std::vector<int>> game_state, int player, int col) {
+
+            for (int row = N_ROW - 1; row >= 0; row--) {
+                if (game_state[row][col] == 0) {
+                    game_state[row][col] = player;
+                    break;
+                }
+            }   
+            return game_state;
+        }
+
+
+        std::vector<int> update_available_moves(std::vector<std::vector<int>> game_state, std::vector<int> available_moves) {
+            
+            for (int j=0; j < N_MOVES; j++) {
+                if (game_state[0][j] > 0) {
+                    available_moves[j] = 0;
+                }
+            }
+            return available_moves;
+        }
+
+
+        int check_state(std::vector<std::vector<int>> game_state) {
+
+            int player, count;
+            int check_draw = 0;
+
+            for (int i = 0; i < N_ROW; i++) {
+                for (int j = 0; j < N_COL; j++) {
+                    if (game_state[i][j] > 0) {
+                        if (i == 0) {
+                            check_draw++;
+                        }
+
+                        player = game_state[i][j];
+
+                        /* Check right */
+                        if (j <= N_COL - CONNECT) {
+                            count = 1;
+
+                            for (int k = 1; k < CONNECT; k++) {
+                                if (game_state[i][j + k] == player) {
+                                    count++;
+                                } else {
+                                    break;
+                                }
+                            }
+                            
+                            if (count == CONNECT) {
+                                return player;
+                            }
+                        }
+
+                        /* Check down */
+                        if (i <= N_ROW - CONNECT) {
+                            count = 1;
+
+                            for (int k = 1; k < CONNECT; k++) {
+                                if (game_state[i + k][j] == player) {
+                                    count++;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            if (count == CONNECT) {
+                                return player;
+                            }
+                        }
+
+                        /* Check right-down */
+                        if (i <= N_ROW - CONNECT && j <= N_COL - CONNECT) {
+                            count = 1;
+                            
+                            for (int k = 1; k < CONNECT; k++) {
+                                if (game_state[i + k][j + k] == player) {
+                                    count++;
+                                } else {
+                                    break;
+                                }
+                            }
+                            
+                            if (count == CONNECT) {
+                                return player;
+                            }
+                        }
+
+                        /* Check left-down */
+                        if (i <= N_ROW - CONNECT && j >= CONNECT - 1) {
+                            count = 1;
+                            
+                            for (int k = 1; k < CONNECT; k++) {
+                                if (game_state[i + k][j - k] == player) {
+                                    count++;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            if (count == CONNECT) {
+                                return player;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (check_draw == N_COL) {
+                /* Declare a draw */
+                return 0;
+            } else {
+                /* Game continues */
+                return -1;
+            }
+        }
+
+
+        double get_score(std::vector<std::vector<int>> game_state, std::vector<int> available_moves, int player, int col, int depth, int max_depth, double ab) {
+
+            game_state = update_state(game_state, player, col);
+            available_moves = update_available_moves(game_state, available_moves);
+            int result = check_state(game_state);
+            double score;
+
+            if (result == 2) {
+                return pow(DECAY, depth);
+            } else if (result == 1) {  
+                return -pow(DECAY, depth);
+            } else if (result == 0) {
+                return 0.0;
+            } else if (depth == max_depth) {  
+                return 0.0;
+            } else {
+                player = switch_player(player);
+
+                if (player == 1) {
+                    
+                    double min_score = S_INITIAL;
+
+                    for (int j = 0; j < N_MOVES; j++) {
+                        if (available_moves[j] == 1) {
+                            score = get_score(game_state, available_moves, player, j, depth + 1, max_depth, min_score);
+
+                            if (score < min_score) {
+                                min_score = score;
+                            }
+
+                            if (min_score <= ab) {
+                                break;
+                            }
+                        }
+                    }
+                    return min_score;
+                }  else /* if (player == 2) */ {
+
+                    double max_score = -S_INITIAL;
+
+                    for (int j = 0; j < N_MOVES; j++) {
+                        if (available_moves[j] == 1) {
+                            score = get_score(game_state, available_moves, player, j, depth + 1, max_depth, max_score);
+
+                            if (score > max_score) {
+                                max_score = score;
+                            }
+
+                            if (max_score >= ab) {
+                                break;
+                            }
+                        }
+                    }
+                    return max_score;
+                }
+            }
+        }
+
+
+        void end_turn(int player, int col) {
+
+            game_state = update_state(game_state, player, col);
+            available_moves = update_available_moves(game_state, available_moves);
+            print_board(game_state, available_moves);
+            result = check_state(game_state);
+            player = switch_player(player);
+        }
+
+};
+
