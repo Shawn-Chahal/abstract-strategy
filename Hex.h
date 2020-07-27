9a47e4933ecc8ee4f754b5a2553bd138b159e0cb
Shawn-Chahal <shawn.chahal@gmail.com>
2020-07-26 21:25:47 -0400

    Worked on Hex.print_board()

--- Hex.h
+++ Hex.h
@@ -7,243 +7,6 @@
 class Hex: public OneStepGame {
     private:
 
-        std::vector<int> check_link(std::vector<std::vector<int>> game_state, int row, int col) {
-            std::vector<int> link = std::vector<int>(8, 0);
-            int player = game_state[row][col];
-            int opponent = switch_player(player);
-
-            // Check right [0]
-            if (col < N_COL - 2) {
-                if (game_state[row][col + 1] == opponent) {
-                    for (int k = 2; col + k < N_COL; k++) {
-                        if (game_state[row][col + k] == 0) {
-                            break;
-                        } else if (game_state[row][col + k] == player) {
-                            link[0] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check up-right [1]
-            if ((row > 1) && (col < N_COL - 2)) {
-                if (game_state[row - 1][col + 1] == opponent) {
-                    for (int k = 2; (row - k >= 0) && (col + k < N_COL); k++) {
-                        if (game_state[row - k][col + k] == 0) {
-                            break;
-                        } else if (game_state[row - k][col + k] == player) {
-                            link[1] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check up [2]
-            if (row > 1) {
-                if (game_state[row - 1][col] == opponent) {
-                    for (int k = 2; row - k >= 0; k++) {
-                        if (game_state[row - k][col] == 0) {
-                            break;
-                        } else if (game_state[row - k][col] == player) {
-                            link[2] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check up-left [3]
-            if ((row > 1) && (col > 1)) {
-                if (game_state[row - 1][col - 1] == opponent) {
-                    for (int k = 2; (row - k >= 0) && (col - k >= 0); k++) {
-                        if (game_state[row - k][col - k] == 0) {
-                            break;
-                        } else if (game_state[row - k][col - k] == player) {
-                            link[3] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check left [4]
-            if (col > 1) {
-                if (game_state[row][col - 1] == opponent) {
-                    for (int k = 2; col - k >= 0; k++) {
-                        if (game_state[row][col - k] == 0) {
-                            break;
-                        } else if (game_state[row][col - k] == player) {
-                            link[4] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check down-left [5]
-            if ((row < N_ROW - 2) && (col > 1)) {
-                if (game_state[row + 1][col - 1] == opponent) {
-                    for (int k = 2; (row + k < N_ROW) && (col - k >= 0); k++) {
-                        if (game_state[row + k][col - k] == 0) {
-                            break;
-                        } else if (game_state[row + k][col - k] == player) {
-                            link[5] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Check down [6]
-            if (row < N_ROW - 2) {
-                if (game_state[row + 1][col] == opponent) {
-                    for (int k = 2; row + k < N_ROW; k++) {
-                        if (game_state[row + k][col] == 0) {
-                            break;
-                        } else if (game_state[row + k][col] == player) {
-                            link[6] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-
-
-            // Check down-right [7]
-            if ((row < N_ROW - 2) && (col < N_COL - 2)) {
-                if (game_state[row + 1][col + 1] == opponent) {
-                    for (int k = 2; (row + k < N_ROW) && (col + k < N_COL); k++) {
-                        if (game_state[row + k][col + k] == 0) {
-                            break;
-                        } else if (game_state[row + k][col + k] == player) {
-                            link[7] = 1;
-                            break;
-                        }
-                    }
-                }
-            }
-            
-            return link;
-        }
-
-
-        int move_available(std::vector<std::vector<int>> game_state, int player, int move) {
-            
-            int row = move / N_COL;
-            int col = move % N_COL;
-            game_state[row][col] = player;
-            int opponent = switch_player(player);
-
-            // Check right [0]
-            if (col < N_COL - 2) {
-                if (game_state[row][col + 1] == opponent) {
-                    for (int k = 2; col + k < N_COL; k++) {
-                        if (game_state[row][col + k] == 0) {
-                            break;
-                        } else if (game_state[row][col + k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check up-right [1]
-            if ((row > 1) && (col < N_COL - 2)) {
-                if (game_state[row - 1][col + 1] == opponent) {
-                    for (int k = 2; (row - k >= 0) && (col + k < N_COL); k++) {
-                        if (game_state[row - k][col + k] == 0) {
-                            break;
-                        } else if (game_state[row - k][col + k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check up [2]
-            if (row > 1) {
-                if (game_state[row - 1][col] == opponent) {
-                    for (int k = 2; row - k >= 0; k++) {
-                        if (game_state[row - k][col] == 0) {
-                            break;
-                        } else if (game_state[row - k][col] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check up-left [3]
-            if ((row > 1) && (col > 1)) {
-                if (game_state[row - 1][col - 1] == opponent) {
-                    for (int k = 2; (row - k >= 0) && (col - k >= 0); k++) {
-                        if (game_state[row - k][col - k] == 0) {
-                            break;
-                        } else if (game_state[row - k][col - k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check left [4]
-            if (col > 1) {
-                if (game_state[row][col - 1] == opponent) {
-                    for (int k = 2; col - k >= 0; k++) {
-                        if (game_state[row][col - k] == 0) {
-                            break;
-                        } else if (game_state[row][col - k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check down-left [5]
-            if ((row < N_ROW - 2) && (col > 1)) {
-                if (game_state[row + 1][col - 1] == opponent) {
-                    for (int k = 2; (row + k < N_ROW) && (col - k >= 0); k++) {
-                        if (game_state[row + k][col - k] == 0) {
-                            break;
-                        } else if (game_state[row + k][col - k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-            // Check down [6]
-            if (row < N_ROW - 2) {
-                if (game_state[row + 1][col] == opponent) {
-                    for (int k = 2; row + k < N_ROW; k++) {
-                        if (game_state[row + k][col] == 0) {
-                            break;
-                        } else if (game_state[row + k][col] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-
-
-            // Check down-right [7]
-            if ((row < N_ROW - 2) && (col < N_COL - 2)) {
-                if (game_state[row + 1][col + 1] == opponent) {
-                    for (int k = 2; (row + k < N_ROW) && (col + k < N_COL); k++) {
-                        if (game_state[row + k][col + k] == 0) {
-                            break;
-                        } else if (game_state[row + k][col + k] == player) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-            
-            return 0;
-        }
-      
 
     public:
         
@@ -254,6 +17,7 @@ class Hex: public OneStepGame {
         const int N_COL = 11;
         const int N_MOVES = N_ROW * N_COL;
         
+        // Need to finish
         double intermediate_score(std::vector<std::vector<int>> game_state) {
             
             int player1_score = 0;
@@ -316,55 +80,53 @@ class Hex: public OneStepGame {
 
             std::vector<std::vector<int>> game_state = std::vector<std::vector<int>>(N_ROW, std::vector<int>(N_COL, 0));
 
-            game_state[N_ROW / 2 - 1][N_COL / 2 - 1] = 2;
-            game_state[N_ROW / 2 - 1][N_COL / 2    ] = 1;
-            game_state[N_ROW / 2    ][N_COL / 2 - 1] = 1;
-            game_state[N_ROW / 2    ][N_COL / 2    ] = 2;
-
             return game_state;
-
-            
         }
 
-
+        // Need to finish
         void print_board(std::vector<std::vector<int>> game_state, std::vector<int> available_moves, const int N_ROW, const int N_COL) {
             
-            int col_index = 0;
+            char col_index = 'a';
 
-            std::cout << INDENT << "      " << col_index++ << std::endl;
-            std::cout << INDENT << "    _____" << std::endl;
+            std::cout << INDENT << "   |XXXXXXX" << std::endl;
+            std::cout << INDENT << "   |OXXX   XXXX" << std::endl;
+
+            std::cout << INDENT << "   |OOX  " << col_index++ << "  XXXXXX" << std::endl;
+            std::cout << INDENT << "   |OOO_____XXX   XXXX" << std::endl;
             
             for (int i = 0; i < N_ROW; i++) {
 
                 // Line 1
                 std::cout << INDENT;
-                if (i < 10) {
-                    std::cout << i << "  ";
-                } else if (i < 100) {
-                    std::cout << i << " ";
+                if (i + 1 < 10) {
+                    std::cout << 0 << i + 1 << " ";
+                } else if (i + 1 < 100) {
+                    std::cout << i + 1 << " ";
                 } else {
-                    std::cout << i;
+                    std::cout << i + 1;
                 }
 
-                std::cout << "/     ";
+                std::cout << "|OO/     ";
 
                 for (int j = 0; (j < i) && (j < N_COL / 2); j++) {
 
                     std::cout << "\\       /     ";
                 }
 
-                std::cout << "\\   ";
+                std::cout << "\\";
 
-                if (col_index < N_COL) {
-                    std::cout << col_index++;
+                if (col_index - 'a' < N_COL) {
+                    std::cout << "X  " << col_index++ << "  XXXXXX";
+                } else if (i >= N_COL / 2) {
+                    std::cout << "OO|";
                 }
                 
-                std::cout <<std::endl;
+                std::cout << std::endl;
                 
 
                 // Line 2
                 std::cout << INDENT;
-                std::cout << "  /       ";
+                std::cout << "   |O/       ";
 
                 for (int j = 0; (j < i) && (j < N_COL / 2); j++) {
                     std::cout << "\\_____/       ";
@@ -372,8 +134,15 @@ class Hex: public OneStepGame {
 
                 std::cout << "\\";
 
-                if (col_index < N_COL) {
-                    std::cout << "_____";
+                if (col_index - 'a' < N_COL) {
+                    std::cout << "_____XXX   XXX";
+                    if (col_index - 'a' == N_COL - 1) {
+                        std::cout << "O|";
+                    } else {
+                        std::cout << "X";
+                    }
+                } else if (i >= N_COL / 2) {
+                    std::cout << "O|";
                 }
                 
                 std::cout <<std::endl;
@@ -381,7 +150,7 @@ class Hex: public OneStepGame {
 
                 // Line 3
                 std::cout << INDENT;
-                std::cout << "  \\       ";
+                std::cout << "   |O\\       ";
 
                 for (int j = 0; (j < i) && (j < N_COL / 2); j++) {
 
@@ -390,8 +159,16 @@ class Hex: public OneStepGame {
 
                 std::cout << "/";
 
-                if (col_index < N_COL) {
-                    std::cout << "     \\   " << col_index++;
+                if (col_index - 'a' < N_COL) {
+                    std::cout << "     \\X  " << col_index++ << "  X";
+                    if (col_index - 'a' == N_COL) {
+                        std::cout << "OO|";
+                    } else {
+                        std::cout << "XXXXX";
+                    }
+                    
+                } else if (i >= N_COL / 2) {
+                    std::cout << "O|";
                 }
                 
                 std::cout <<std::endl;
@@ -399,7 +176,7 @@ class Hex: public OneStepGame {
 
                 // Line 4
                 std::cout << INDENT;
-                std::cout << "   \\_____/";
+                std::cout << "   |OO\\_____/";
 
                 for (int j = 0; (j < i) && (j < N_COL / 2); j++) {
                     std::cout << "       \\_____/";
@@ -407,13 +184,23 @@ class Hex: public OneStepGame {
 
                 
 
-                if (col_index <= N_COL) {
-                    std::cout << "       \\_____";
-                    
-                    if (col_index == N_COL) {
-                        col_index++;
+                if (col_index - 'a' < N_COL) {
+                    std::cout << "       \\_____XXX   XXXX"; 
+                } else if (col_index - 'a' == N_COL) {
+                    std::cout << "       \\_____OOO|";
+                    col_index++;
+                } else if (i >= N_COL / 2) {
+                    if (i + 1 - N_COL / 2 < 10) {
+                        std::cout << "OO| " << 0 << i + 1 - N_COL / 2;
+                    } else if (i + 1 < 100) {
+                        std::cout << "OO| " << i + 1 - N_COL / 2;
+                    } else {
+                        std::cout << "OO|" << i + 1 - N_COL / 2;
                     }
                 }
+
+
+                
                 
                 std::cout <<std::endl;
             
@@ -429,96 +216,6 @@ class Hex: public OneStepGame {
             int col = move % N_COL;
 
             game_state[row][col] = player;
-
-            std::vector<int> link = check_link(game_state, row, col);
-
-            // Check right [0]
-            if (link[0]) {
-                for (int k = 1; col + k < N_COL; k++) {
-                    if (game_state[row][col + k] == player) {
-                        break;
-                    } else {
-                        game_state[row][col + k] = player;
-                    }
-                }
-            }
-
-            // Check up-right [1]
-            if (link[1]) {
-                for (int k = 1; (row - k >= 0) && (col + k < N_COL); k++) {
-                    if (game_state[row - k][col + k] == player) {
-                        break;
-                    } else {
-                        game_state[row - k][col + k] = player;
-                    }
-                }
-            }
-
-            // Check up [2]
-            if (link[2]) {
-                for (int k = 1; row - k >= 0; k++) {
-                    if (game_state[row - k][col] == player) {
-                        break;
-                    } else {
-                        game_state[row - k][col] = player;
-                    }
-                }
-            }
-
-            // Check up-left [3]
-            if (link[3]) {
-                for (int k = 1; (row - k >= 0) && (col - k >= 0); k++) {
-                    if (game_state[row - k][col - k] == player) {
-                        break;
-                    } else {
-                        game_state[row - k][col - k] = player;
-                    }
-                }
-            }
-
-            // Check left [4]
-            if (link[4]) {
-                for (int k = 1; col - k >= 0; k++) {
-                    if (game_state[row][col - k] == player) {
-                        break;
-                    } else {
-                        game_state[row][col - k] = player;
-                    }
-                }
-            }
-
-            // Check down-left [5]
-            if (link[5]) {
-                for (int k = 1; (row + k < N_ROW) && (col - k >= 0); k++) {
-                    if (game_state[row + k][col - k] == player) {
-                        break;
-                    } else {
-                        game_state[row + k][col - k] = player;
-                    }
-                }
-            }
-
-            // Check down [6]
-            if (link[6]) {
-                for (int k = 1; row + k < N_ROW; k++) {
-                    if (game_state[row + k][col] == player) {
-                        break;
-                    } else {
-                        game_state[row + k][col] = player;
-                    }
-                }
-            }
-
-            // Check down-right [7]
-            if (link[7]) {
-                for (int k = 1; (row + k < N_ROW) && (col + k < N_COL); k++) {
-                    if (game_state[row + k][col + k] == player) {
-                        break;
-                    } else {
-                        game_state[row + k][col + k] = player;
-                    }
-                }
-            }
             
             return game_state;
         }
@@ -530,17 +227,14 @@ class Hex: public OneStepGame {
 
             for (int m = 0; m < N_MOVES; m++) {
                 if (game_state[m / N_COL][m % N_COL] == 0) {
-                    if (move_available(game_state, player, m)) {
-                        available_moves[m] = 1;
-
-                    }
+                    available_moves[m] = 1;
                 }
             }
 
             return available_moves;
         }
 
-
+        // Need to finish
         int get_result(std::vector<std::vector<int>> game_state) {
 
             int player1_score = 0;
